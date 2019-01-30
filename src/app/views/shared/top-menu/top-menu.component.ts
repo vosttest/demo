03d2cb577009ba12dd0bcbfaf3d90c6eeff8a058
Hoan Nguyen <32765877+hoanpoli@users.noreply.github.com>
2019-01-30 11:05:53 +0700

    VOST 163

--- src/app/views/shared/top-menu/top-menu.component.ts
+++ src/app/views/shared/top-menu/top-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { strictEqual } from 'assert';
 
 @Component({
   selector: 'app-top-menu',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-menu.component.css']
 })
 export class TopMenuComponent implements OnInit {
+  hideLogo: String = null;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.router.events.subscribe((rsp: any) => {
+      const url: String = rsp.url;
+      if (url != null) {
+        if (url.includes('/news'))
+          this.hideLogo = 'hide-logo';
+        else
+          this.hideLogo = null;
+        return;
+      }
+    });
   }
 
 }
